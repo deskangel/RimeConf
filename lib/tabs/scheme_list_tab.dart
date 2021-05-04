1f331c1b8f60b5f005f823659e3e4eff50ff734d
WilliamX <williamx@deskangel.com>
2021-05-04 23:27:50 +0800

    * updated styles

--- lib/tabs/scheme_list_tab.dart
+++ lib/tabs/scheme_list_tab.dart
@@ -13,15 +13,23 @@ class _SchemeListTabState extends State<SchemeListTab> {
   final ConfCtrl ctrl = Get.put(ConfCtrl());
 
   @override
+  void initState() {
+    super.initState();
+    ctrl.readConf();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        ElevatedButton(
-          onPressed: () => ctrl.readConf(),
-          child: Obx(() => Text('${ctrl.length.value}')),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            '输入方案列表：',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
         ),
-        Text('输入方案列表：'),
         Expanded(
           child: GetBuilder<ConfCtrl>(
             init: ConfCtrl(),
@@ -32,7 +40,10 @@ class _SchemeListTabState extends State<SchemeListTab> {
                 itemCount: ctrl.length.value,
                 itemBuilder: (BuildContext context, int index) {
                   return ListTile(
-                    title: Text(ctrl.schemeList![index]),
+                    minVerticalPadding: 1,
+                    // contentPadding: EdgeInsets.zero,
+                    dense: true,
+                    title: Text(ctrl.schemeList![index], style: TextStyle(fontSize: 16)),
                     trailing: IconButton(
                       icon: Icon(Icons.cancel_outlined),
                       onPressed: () {
